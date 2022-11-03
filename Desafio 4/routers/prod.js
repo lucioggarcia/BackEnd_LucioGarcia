03d352127f414b03cefed48cf00302a22887ad1c
Lucio Garcia <garcialuciogonzalo@gmail.com>
2022-11-02 21:43:40 -0300

    arreglo

--- Desafio 4/routers/prod.js
+++ Desafio 4/routers/prod.js
@@ -1,7 +1,7 @@
 const { Router, request } = require("express");
 const router = Router();
-const Contenedor = require("../controllers/contenedor");
-const productos = new Contenedor("../controllers/productos.json");
+const Contenedor = require("../controllers/contenedor.js");
+const productos = new Contenedor('./controllers/productos.json');
 const notFound = { error: "Producto no encontrado" };
 
 router.get("/", async (req, res) => {
